046e0f301bb7dab69a2e918c4251266e4c72afa3
Simon Horne <s.horne@packetizer.com>
2010-08-19 12:45:13 +0000

    Improved Generic Capability merge

--- src/mediafmt.cxx
+++ src/mediafmt.cxx
@@ -24,6 +24,9 @@
  * Contributor(s): ______________________________________.
  *
  * $Log$
+ * Revision 1.7  2010/06/06 14:49:44  shorne
+ * G.711 codec now 20ms (was 30ms) sample size
+ *
  * Revision 1.6  2010/05/02 22:48:12  shorne
  * Added support to order generic parameters in capablity exchange. Added G.711 20ms codec
  *
@@ -425,6 +428,15 @@ PObject::Comparison OpalMediaOption::Compare(const PObject & obj) const
 
 bool OpalMediaOption::Merge(const OpalMediaOption & option)
 {
+  // Generic Parameters that are default zero are place holders and you merge
+  // to the value of the other party
+  if ((option.GetH245Generic().mode != OpalMediaOption::H245GenericInfo::None) &&
+      PIsDescendant(&option, OpalMediaOptionUnsigned)) {
+       if (((OpalMediaOptionUnsigned *)this)->GetValue() == 0) {
+          Assign(option); return true;
+       } 
+  }
+
   switch (m_merge) {
     case MinMerge :
       if (CompareValue(option) == GreaterThan)
