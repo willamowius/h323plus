b5987947ae47f1f76dbfb2c54e5a498c41ed1518
Simon Horne <s.horne@packetizer.com>
2011-07-30 04:31:42 +0000

    Inremented to 1.23

--- version.h
+++ version.h
@@ -26,9 +26,9 @@
  */
 
 #define MAJOR_VERSION 1
-#define MINOR_VERSION 22
+#define MINOR_VERSION 23
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER 2
+#define BUILD_NUMBER 0
 
 
 #endif  // _H323PLUS_VERSION_H
