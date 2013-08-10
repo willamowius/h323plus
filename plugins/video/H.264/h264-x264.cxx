85d38beee3a45e44a39ad0c58061ab38f1814aab
Simon Horne <s.horne@packetizer.com>
2013-08-10 17:31:53 +0000

    Fix typo

--- plugins/video/H.264/h264-x264.cxx
+++ plugins/video/H.264/h264-x264.cxx
@@ -1265,7 +1265,7 @@ static int encoder_set_options(
 		  if (STRCMPI(options[i], PLUGINCODEC_OPTION_TARGET_BIT_RATE) == 0)
 			 options[i+1] = num2str(targetBitrate);	
 		  if (STRCMPI(options[i], PLUGINCODEC_OPTION_FRAME_TIME) == 0)
-			 options[i+1] = framerate > 0 ? num2str(H264_CLOCKRATE/frameRate) : num2str(3000);
+			 options[i+1] = frameRate > 0 ? num2str(H264_CLOCKRATE/frameRate) : num2str(3000);
 		  if (STRCMPI(options[i], PLUGINCODEC_OPTION_FRAME_HEIGHT) == 0)
 			 options[i+1] = num2str(frameHeight);
 		  if (STRCMPI(options[i], PLUGINCODEC_OPTION_FRAME_WIDTH) == 0)
