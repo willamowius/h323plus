bfa9a55718183886b7b3bd53e04c47765916ed68
Simon Horne <s.horne@packetizer.com>
2013-12-10 12:51:29 +0000

    Do not include TLS connectionAddress in either the GRQ or ARQ to the gatekeeper

--- src/h460/h460_std22.cxx
+++ src/h460/h460_std22.cxx
@@ -99,7 +99,7 @@ PBoolean H460_FeatureStd22::FeatureAdvertised(int mtype)
      }
 }
 
-void BuildFeature(H323TransportSecurity * transec, H323EndPoint * ep, H460_FeatureStd & feat, PBoolean grq = false)
+void BuildFeature(H323TransportSecurity * transec, H323EndPoint * ep, H460_FeatureStd & feat, PBoolean address = true)
 {
 
     if (transec->IsTLSEnabled()) {
@@ -107,7 +107,7 @@ void BuildFeature(H323TransportSecurity * transec, H323EndPoint * ep, H460_Featu
         if (tls) {
             H460_FeatureStd sets;
             sets.Add(Std22_Priority,H460_FeatureContent(1,8)); // Priority 1
-            if (!grq)
+            if (address)
                 sets.Add(Std22_Address,H460_FeatureContent(tls->GetTransportAddress()));
             feat.Add(Std22_TLS,H460_FeatureContent(sets.GetCurrentTable()));
         }
@@ -143,7 +143,7 @@ PBoolean H460_FeatureStd22::OnSendGatekeeperRequest(H225_FeatureDescriptor & pdu
         return false;
 
     H460_FeatureStd feat = H460_FeatureStd(22);  
-    BuildFeature(EP->GetTransportSecurity(), EP, feat, true);
+    BuildFeature(EP->GetTransportSecurity(), EP, feat, false);
 
     pdu = feat;
 	return true;
@@ -189,7 +189,7 @@ PBoolean H460_FeatureStd22::OnSendAdmissionRequest(H225_FeatureDescriptor & pdu)
         return false;
 
     H460_FeatureStd feat = H460_FeatureStd(22);
-    BuildFeature(&m_supportedSecurity, EP, feat);
+    BuildFeature(&m_supportedSecurity, EP, feat, false);
     pdu = feat;
 
     return true;
