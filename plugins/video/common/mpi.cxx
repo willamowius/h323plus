4901ccb4ba865f67fe27b4412c4356da6888bbbd
Jan Willamowius <jan@willamowius.de>
2019-03-27 09:21:29 +0100

    help newer gcc versions to pick the right prototype for abs()

--- plugins/video/common/mpi.cxx
+++ plugins/video/common/mpi.cxx
@@ -132,8 +132,8 @@ bool MPIList::getNegotiatedMPI( unsigned* width, unsigned* height, unsigned* _fr
   // to the desired one or matches it
   for (i=0; i < MPIs.size(); i++) {
     // we square the value in order to get absolute distances
-    distance = ( abs(MPIs[i].width  - desiredWidth ) *
-                 abs(MPIs[i].height - desiredHeight) );
+    distance = ( abs(int(MPIs[i].width  - desiredWidth) ) *
+                 abs(int(MPIs[i].height - desiredHeight)) );
 
     if (distance < minDistance) {
       minDistance = distance;
