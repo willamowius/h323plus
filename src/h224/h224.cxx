3ec72b1867bb5958777901719652c97be72efa11
Jan Willamowius <jan@willamowius.de>
2021-02-06 14:21:33 +0100

    initialize all members

--- src/h224/h224.cxx
+++ src/h224/h224.cxx
@@ -48,20 +48,20 @@ H224_Frame::H224_Frame(PINDEX size)
 : Q922_Frame(H224_HEADER_SIZE + size)
 {
   SetHighPriority(FALSE);
-    
+
   SetControlFieldOctet(0x03);
-    
+
   BYTE *data = GetInformationFieldPtr();
-    
+
   // setting destination & source terminal address to BROADCAST
   data[0] = 0;
   data[1] = 0;
   data[2] = 0;
   data[3] = 0;
-    
+
   // setting Client ID to CME
   data[4] = 0;
-    
+
   // setting ES / BS / C1 / C0 / Segment number to zero
   data[5] = 0;
 }
@@ -73,7 +73,7 @@ H224_Frame::~H224_Frame()
 void H224_Frame::SetHighPriority(PBoolean flag)
 {
   SetHighOrderAddressOctet(0x00);
-    
+
   if(flag) {
     SetLowOrderAddressOctet(0x71);
   } else {
@@ -110,7 +110,7 @@ void H224_Frame::SetSourceTerminalAddress(WORD address)
 BYTE H224_Frame::GetClientID() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return data[4] & 0x7f;
 }
 
@@ -118,21 +118,21 @@ void H224_Frame::SetClientID(BYTE clientID)
 {
 
   BYTE *data = GetInformationFieldPtr();
-    
+
   data[4] = clientID;
 }
 
 PBoolean H224_Frame::GetBS() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return (data[5] & 0x80) != 0;
 }
 
 void H224_Frame::SetBS(PBoolean flag)
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   if(flag) {
     data[5] |= 0x80;
   }    else {
@@ -143,14 +143,14 @@ void H224_Frame::SetBS(PBoolean flag)
 PBoolean H224_Frame::GetES() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return (data[5] & 0x40) != 0;
 }
 
 void H224_Frame::SetES(PBoolean flag)
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   if(flag) {
     data[5] |= 0x40;
   } else {
@@ -161,14 +161,14 @@ void H224_Frame::SetES(PBoolean flag)
 PBoolean H224_Frame::GetC1() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return (data[5] & 0x20) != 0;
 }
 
 void H224_Frame::SetC1(PBoolean flag)
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   if(flag) {
     data[5] |= 0x20;
   } else {
@@ -179,14 +179,14 @@ void H224_Frame::SetC1(PBoolean flag)
 PBoolean H224_Frame::GetC0() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return (data[5] & 0x10) != 0;
 }
 
 void H224_Frame::SetC0(PBoolean flag)
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   if(flag) {
     data[5] |= 0x10;
   }    else {
@@ -197,40 +197,40 @@ void H224_Frame::SetC0(PBoolean flag)
 BYTE H224_Frame::GetSegmentNumber() const
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   return (data[5] & 0x0f);
 }
 
 void H224_Frame::SetSegmentNumber(BYTE segmentNumber)
 {
   BYTE *data = GetInformationFieldPtr();
-    
+
   data[5] &= 0xf0;
   data[5] |= (segmentNumber & 0x0f);
 }
 
-PBoolean H224_Frame::Decode(const BYTE *data, 
+PBoolean H224_Frame::Decode(const BYTE *data,
                         PINDEX size)
 {
   PBoolean result = Q922_Frame::Decode(data, size);
-    
+
   if(result == FALSE) {
     return FALSE;
   }
-    
+
   // doing some validity check for H.224 frames
   BYTE highOrderAddressOctet = GetHighOrderAddressOctet();
   BYTE lowOrderAddressOctet = GetLowOrderAddressOctet();
   BYTE controlFieldOctet = GetControlFieldOctet();
-    
+
   if((highOrderAddressOctet != 0x00) ||
      (!(lowOrderAddressOctet == 0x61 || lowOrderAddressOctet == 0x71)) ||
      (controlFieldOctet != 0x03) ||
      (GetClientID() > 0x03))
-  {        
+  {
       return FALSE;
   }
-    
+
   return TRUE;
 }
 
@@ -317,19 +317,20 @@ inline void DeleteObjectsInMap(M & m)
 OpalH224Handler::OpalH224Handler(H323Channel::Directions dir,
                                  H323Connection & connection,
                                  unsigned sessionID)
-: session(NULL), canTransmit(FALSE), transmitMutex(), sessionDirection(dir)
+: session(NULL), canTransmit(FALSE), transmitMutex(),
+  transmitFrame(NULL), transmitBitIndex(0), transmitStartTime(NULL),
+  sessionDirection(dir)
 #ifdef H323_H235
   ,secChannel(NULL)
 #endif
 {
-
   H245_TransportAddress addr;
   connection.GetControlChannel().SetUpTransportPDU(addr, H323Transport::UseLocalTSAP);
   session = connection.UseSession(sessionID,addr,H323Channel::IsBidirectional);
 
   CreateHandlers(connection);
   receiverThread = NULL;
-    
+
 }
 
 OpalH224Handler::~OpalH224Handler()
@@ -345,7 +346,7 @@ void OpalH224Handler::CreateHandlers(H323Connection & connection)
         H224_Handler * handler = connection.CreateH224Handler(sessionDirection, *this, handlers[i]);
         if (!handler) {
             handler = H224_Handler::CreateHandler(handlers[i]);
-            if (handler) 
+            if (handler)
                 handler->AttachH224Handler(this);
         }
         if (!handler) continue;
@@ -369,20 +370,20 @@ void OpalH224Handler::DeleteHandlers()
 void OpalH224Handler::StartTransmit()
 {
   PWaitAndSignal m(transmitMutex);
-    
+
   if(canTransmit == TRUE) {
     return;
   }
-    
+
   canTransmit = TRUE;
-    
+
   transmitFrame = new RTP_DataFrame(300);
-  
+
   // Use payload code 100 as this seems to be common to other implementations
   transmitFrame->SetPayloadType((RTP_DataFrame::PayloadTypes)100);
   transmitBitIndex = 7;
   transmitStartTime = new PTime();
-    
+
   PThread::Sleep(2000); // delay sending info for NAT Probs and remote can create theirs
   SendClientList();
   SendExtraCapabilities();
@@ -391,10 +392,10 @@ void OpalH224Handler::StartTransmit()
 void OpalH224Handler::StopTransmit()
 {
   PWaitAndSignal m(transmitMutex);
-    
+
   delete transmitStartTime;
   transmitStartTime = NULL;
-    
+
   canTransmit = FALSE;
 }
 
@@ -404,7 +405,7 @@ void OpalH224Handler::StartReceive()
     PTRACE(5, "H.224 handler is already receiving");
     return;
   }
-    
+
   receiverThread = CreateH224ReceiverThread();
   receiverThread->Resume();
 }
@@ -425,7 +426,7 @@ int CalculateClientListSize(const std::map<BYTE,H224_Handler*> & handlers)
           i += 2;
         } else if(clientID == 0x7f) { // non-standard client ID
           i += 6;
-        } else { // standard client ID 
+        } else { // standard client ID
           i++;
         }
     }
@@ -441,24 +442,24 @@ PBoolean OpalH224Handler::SendClientList()
   int count = m_h224Handlers.size();
   if (!count)
     return false;
-    
+
   H224_Frame h224Frame = H224_Frame(CalculateClientListSize(m_h224Handlers));
   h224Frame.SetHighPriority(TRUE);
   h224Frame.SetDestinationTerminalAddress(H224_BROADCAST);
   h224Frame.SetSourceTerminalAddress(H224_BROADCAST);
-    
+
   // CME frame
   h224Frame.SetClientID(0x00);
-    
+
   // Begin and end of sequence
   h224Frame.SetBS(TRUE);
   h224Frame.SetES(TRUE);
   h224Frame.SetC1(FALSE);
   h224Frame.SetC0(FALSE);
   h224Frame.SetSegmentNumber(0);
-    
+
   BYTE *ptr = h224Frame.GetClientDataPtr();
-    
+
   ptr[0] = 0x01; // Client list code
   ptr[1] = 0x00; // Message code
   ptr[2] = (BYTE)count; // client count
@@ -471,13 +472,13 @@ PBoolean OpalH224Handler::SendClientList()
               i += 2;
             } else if(clientID == 0x7f) { // non-standard client ID
               i += 6;
-            } else { // standard client ID 
+            } else { // standard client ID
               i++;
             }
         }
     }
   TransmitFrame(h224Frame);
-    
+
   return TRUE;
 }
 
@@ -488,48 +489,48 @@ PBoolean OpalH224Handler::SendExtraCapabilities()
 
   //handlersMutex.Wait();
   for (std::map<BYTE,H224_Handler*>::iterator it = m_h224Handlers.begin(); it != m_h224Handlers.end(); ++it) {
-    if (it->second->IsActive(sessionDirection)) 
+    if (it->second->IsActive(sessionDirection))
         it->second->SendExtraCapabilities();
   }
   //handlersMutex.Signal();
-    
+
   return TRUE;
 }
 
-PBoolean OpalH224Handler::SendExtraCapabilitiesMessage(BYTE clientID, 
+PBoolean OpalH224Handler::SendExtraCapabilitiesMessage(BYTE clientID,
                                                    BYTE *data, PINDEX length)
-{    
+{
   PWaitAndSignal m(transmitMutex);
-    
+
   if(canTransmit == FALSE)
     return FALSE;
-    
+
   H224_Frame h224Frame = H224_Frame(length+3);
   h224Frame.SetHighPriority(TRUE);
   h224Frame.SetDestinationTerminalAddress(H224_BROADCAST);
   h224Frame.SetSourceTerminalAddress(H224_BROADCAST);
-    
+
   // use clientID zero to indicate a CME frame
   h224Frame.SetClientID(0x00);
-    
+
   // Begin and end of sequence, rest is zero
   h224Frame.SetBS(TRUE);
   h224Frame.SetES(TRUE);
   h224Frame.SetC1(FALSE);
   h224Frame.SetC0(FALSE);
   h224Frame.SetSegmentNumber(0);
-    
+
   BYTE *ptr = h224Frame.GetClientDataPtr();
-    
+
   ptr[0] = 0x02; // Extra Capabilities code
   ptr[1] = 0x00; // Response Code
   ptr[2] = (0x80 | clientID); // EX CAPS and ClientID
-    
+
   memcpy(ptr+3, data, length);
-    
+
   TransmitFrame(h224Frame);
-    
-  return TRUE;    
+
+  return TRUE;
 }
 
 PBoolean OpalH224Handler::TransmitClientFrame(BYTE clientID, H224_Frame & frame, PBoolean replay)
@@ -539,11 +540,11 @@ PBoolean OpalH224Handler::TransmitClientFrame(BYTE clientID, H224_Frame & frame,
     return FALSE;
 
   PWaitAndSignal m(transmitMutex);
-    
+
   frame.SetClientID(clientID);
-    
+
   TransmitFrame(frame,replay);
-    
+
   return TRUE;
 }
 
@@ -555,13 +556,13 @@ PBoolean OpalH224Handler::OnReceivedFrame(H224_Frame & frame)
     return TRUE;
   }
   BYTE clientID = frame.GetClientID();
-    
+
   if(clientID == 0x00) {
     return OnReceivedCMEMessage(frame);
   }
 
   PTRACE(5, "H224\tReceived frame for ClientID " << clientID);
-    
+
   handlersMutex.Wait();
   for (std::map<BYTE,H224_Handler*>::iterator it = m_h224Handlers.begin(); it != m_h224Handlers.end(); ++it) {
       if (clientID == it->first)    {
@@ -571,33 +572,33 @@ PBoolean OpalH224Handler::OnReceivedFrame(H224_Frame & frame)
   }
   handlersMutex.Signal();
 
-    
+
   return TRUE;
 }
 
 PBoolean OpalH224Handler::OnReceivedCMEMessage(H224_Frame & frame)
 {
   BYTE *data = frame.GetClientDataPtr();
-    
+
   if(data[0] == 0x01) { // Client list code
-    
+
     if(data[1] == 0x00) { // Message
       return OnReceivedClientList(frame);
-        
+
     } else if(data[1] == 0xff) { // Command
       return OnReceivedClientListCommand();
     }
-      
+
   } else if(data[0] == 0x02) { // Extra Capabilities code
-      
+
     if(data[1] == 0x00) { // Message
       return OnReceivedExtraCapabilities(frame);
-        
+
     } else if(data[1] == 0xff) {// Command
       return OnReceivedExtraCapabilitiesCommand();
     }
   }
-    
+
   // incorrect frames are simply ignored
   return TRUE;
 }
@@ -605,13 +606,13 @@ PBoolean OpalH224Handler::OnReceivedCMEMessage(H224_Frame & frame)
 PBoolean OpalH224Handler::OnReceivedClientList(H224_Frame & frame)
 {
   BYTE *data = frame.GetClientDataPtr();
-    
+
   BYTE numberOfClients = data[2];
-    
+
   PINDEX i = 3;
-    
+
   while(numberOfClients > 0) {
-      
+
     BYTE clientID = (BYTE)(data[i] & 0x7f);
 
     for (std::map<BYTE,H224_Handler*>::iterator it = m_h224Handlers.begin(); it != m_h224Handlers.end(); ++it) {
@@ -620,17 +621,17 @@ PBoolean OpalH224Handler::OnReceivedClientList(H224_Frame & frame)
         break;
       }
     }
-        
+
     if(clientID == 0x7e) { // extended client ID
       i += 2;
     } else if(clientID == 0x7f) { // non-standard client ID
       i += 6;
-    } else { // standard client ID 
+    } else { // standard client ID
       i++;
     }
     numberOfClients--;
   }
-    
+
   return TRUE;
 }
 
@@ -643,9 +644,9 @@ PBoolean OpalH224Handler::OnReceivedClientListCommand()
 PBoolean OpalH224Handler::OnReceivedExtraCapabilities(H224_Frame & frame)
 {
   BYTE *data = frame.GetClientDataPtr();
-    
+
   BYTE clientID = (data[2] & 0x7f);
-    
+
   //handlersMutex.Wait();
   for (std::map<BYTE,H224_Handler*>::iterator it = m_h224Handlers.begin(); it != m_h224Handlers.end(); ++it) {
       if (clientID == it->first) {
@@ -654,7 +655,7 @@ PBoolean OpalH224Handler::OnReceivedExtraCapabilities(H224_Frame & frame)
       }
   }
   //handlersMutex.Signal();
-    
+
   return TRUE;
 }
 
@@ -670,7 +671,7 @@ OpalH224ReceiverThread * OpalH224Handler::CreateH224ReceiverThread()
 }
 
 void OpalH224Handler::TransmitFrame(H224_Frame & frame, PBoolean replay)
-{    
+{
     if (!replay) {
         PINDEX size = frame.GetEncodedSize();
 
@@ -687,7 +688,7 @@ void OpalH224Handler::TransmitFrame(H224_Frame & frame, PBoolean replay)
         transmitFrame->SetPayloadSize(size);
         transmitFrame->SetMarker(TRUE);
     }
-  
+
     // TODO: Add Encryption Support - SH
     if(!session || !session->PreWriteData(*transmitFrame) || !OnWriteFrame(*transmitFrame) || !session->WriteData(*transmitFrame)) {
         PTRACE(3, "H224\tFailed to write encoded H.224 frame");
@@ -712,7 +713,7 @@ PBoolean OpalH224Handler::OnReadFrame(RTP_DataFrame & frame)
 #endif
         return true;
 }
-  
+
 PBoolean OpalH224Handler::OnWriteFrame(RTP_DataFrame & frame)
 {
 #ifdef H323_H235
@@ -740,19 +741,19 @@ OpalH224ReceiverThread::~OpalH224ReceiverThread()
 void OpalH224ReceiverThread::Main()
 {
   RTP_DataFrame packet = RTP_DataFrame(300);
-  H224_Frame h224Frame = H224_Frame(); 
-  unsigned timestamp = 0; 
+  H224_Frame h224Frame = H224_Frame();
+  unsigned timestamp = 0;
 
   threadClosed = false;
 
   while (!exitReceive.Wait(0)) {
 
-    if(!rtpSession.ReadBufferedData(timestamp, packet)) 
+    if(!rtpSession.ReadBufferedData(timestamp, packet))
         break;
 
     if (!h224Handler->OnReadFrame(packet))
         continue;
- 
+
     timestamp = packet.GetTimestamp();
     if (timestamp == lastTimeStamp)
         continue;
