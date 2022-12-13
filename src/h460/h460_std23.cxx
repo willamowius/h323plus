515fb9977826075f6bf7707c6bc661e6c96c5521
Jan Willamowius <jan@willamowius.de>
2022-12-13 12:55:08 +0100

    the H.460.23/.24 patent expired

--- src/h460/h460_std23.cxx
+++ src/h460/h460_std23.cxx
@@ -53,12 +53,6 @@
  #include "h460/upnpcp.h"
 #endif
 
-#if _WIN32
-#pragma message("H.460.23/.24 Enabled. Contact support@h323plus.org for licensing terms.")
-#else
-#warning("H.460.23/.24 Enabled. Contact support@h323plus.org for licensing terms.")
-#endif
-
 #ifdef _MSC_VER
 #pragma warning(disable : 4239)
 #endif
